e40d66b1925db0e0d4b48b4aa21a28d011661620
Juan Andrade <jpas.juan23@gmail.com>
2021-06-18 21:20:50 -0500

    Module to validate numbers.

---
+++ validation_num.py
@@ -0,0 +1,19 @@
+class Validation:
+    """Valid data entry by user."""
+    def __init__(self):
+        self.message = str
+
+    def validated_float(self, message):
+        self.message = message
+        while True:
+            try:
+                validated_value = input(self.message).replace(' ', '')
+                if validated_value == '':
+                    return ''
+                else:
+                    validated_value = float(validated_value)
+                    validated_value = round(validated_value, 2)
+                    break
+            except ValueError:
+                print(' ingrese un dato correcto...\n')
+        return validated_value
