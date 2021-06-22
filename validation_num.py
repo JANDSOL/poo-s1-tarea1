3f19f8e5ea86b856ce82e611206fb2850b2a795d
Juan Andrade <jpas.juan23@gmail.com>
2021-06-22 12:59:31 -0500

    New method to validate an integer in validation_num.py

--- validation_num.py
+++ validation_num.py
@@ -17,3 +17,18 @@ class Validation:
             except ValueError:
                 print(' ingrese un dato correcto...\n')
         return validated_value
+    
+    def validated_int(self, message):
+        self.message = message
+        while True:
+            try:
+                validated_value = input(self.message).replace(' ', '')
+                if validated_value == '':
+                    return ''
+                if '.' in validated_value:
+                    validated_value = float(validated_value)
+                validated_value = int(validated_value)
+                break
+            except ValueError:
+                print(' ingrese un dato correcto...\n')
+        return validated_value
